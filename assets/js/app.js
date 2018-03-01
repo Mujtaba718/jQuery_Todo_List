a33d2cdb84564c7646184b9fbbabcf95fcb9bba0
Mujtaba Iqbal <mujtaba.iq@gmail.com>
2018-03-01 13:12:55 -0500

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed")
+});
+
+// Click on X to delete
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		// grabbing new todo text from input 
+		var todoText = $(this).val();
+		// Empty input field
+		$(this).val("");	
+		// create a new li to and to ul 
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
